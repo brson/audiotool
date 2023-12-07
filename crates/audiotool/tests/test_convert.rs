32619c171944755e34b281438111f452c5f7b8b8
Brian Anderson <andersrb@gmail.com>
2023-12-07 01:24:59 -0700

    .

--- crates/audiotool/tests/test_convert.rs
+++ crates/audiotool/tests/test_convert.rs
@@ -1,6 +1,21 @@
 use rx::prelude::*;
 use audiotool::convert as cvt;
 use audiotool::types::*;
+use audiotool::io::{Props, Buf};
+use std::path::Path;
+
+fn write_test_file(
+    path: &Path,
+    format: Format,
+    channels: u16,
+    frames: u32,
+) -> AnyResult<Buf> {
+    todo!()
+}
+
+fn load_file(path: &Path) -> (Props, Buf) {
+    todo!()
+}
 
 #[test]
 fn basic() -> AnyResult<()> {
@@ -13,11 +28,51 @@ fn basic() -> AnyResult<()> {
         formats: vec![
             Format {
                 codec: Codec::Wav,
-                bit_depth: BitDepth::I24,
+                bit_depth: BitDepth::F32,
                 sample_rate: SampleRate::K48,
             },
         ]
     };
+
+    let informat = Format {
+        codec: Codec::Wav,
+        bit_depth: BitDepth::F32,
+        sample_rate: SampleRate::K48,
+    };
+    let infile = config.reference_tracks_dir.join("test.wav");
+    let outfile = config.out_root_dir.join("test.wav");
+
+    let channels = 2;
+    let frames = 1024;
+
+    write_test_file(&infile, informat, 2, 1024)?;
+
+    let (tx, rx) = cvt::plan::spawn(config);
+    let plan = match rx.recv().expect("recv") {
+        cvt::plan::Response::Done(Ok(Some(plan))) => plan,
+        cvt::plan::Response::Done(Ok(None)) => panic!(),
+        cvt::plan::Response::Done(Err(e)) => panic!("{e}"),
+    };
+    
+    let (tx, rx) = cvt::exec::spawn(plan);
+
+    loop {
+        let resp = rx.recv()?;
+
+        match resp {
+            cvt::exec::Response::NextResult(res) => {
+                //println!("{res:#?}");
+            }
+            cvt::exec::Response::Done => {
+                break;
+            }
+            cvt::exec::Response::Cancelled => {
+                panic!();
+            }
+        }
+    }
+
+    
 
     todo!()
 }
