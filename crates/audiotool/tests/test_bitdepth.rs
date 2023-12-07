fef5506e4174aa031e9aa204d50f718de2305f66
Brian Anderson <andersrb@gmail.com>
2023-12-07 15:22:39 -0700

    .

--- crates/audiotool/tests/test_bitdepth.rs
+++ crates/audiotool/tests/test_bitdepth.rs
@@ -13,6 +13,15 @@ fn do_i24_to_f32_roundtrip(i1: i32) {
     assert_eq!(i1, i2);
 }
 
+fn do_i16_to_i24_via_f32_roundtrip(i1: i16) {
+    let f1 = i16_to_f32(i1);
+    let i2 = f32_to_i24(f1);
+    let f2 = i24_to_f32(i2);
+    let i3 = f32_to_i16(f2);
+    eprintln!("{i1} {f1} {i2} {f2} {i3}");
+    assert_eq!(i1, i3);
+}
+
 #[test]
 fn i16_to_f32_roundtrips() {
     do_i16_to_f32_roundtrip(0);
@@ -25,6 +34,13 @@ fn i24_to_f32_roundtrips() {
     do_i24_to_f32_roundtrip(0);
     do_i24_to_f32_roundtrip(I24_MIN);
     do_i24_to_f32_roundtrip(I24_MAX);
+}
+
+#[test]
+fn i16_to_i24_via_f32_roundtrips() {
+    do_i16_to_i24_via_f32_roundtrip(0);
+    do_i16_to_i24_via_f32_roundtrip(i16::MIN);
+    do_i16_to_i24_via_f32_roundtrip(i16::MAX);
 }
 
 proptest! {
@@ -43,4 +59,12 @@ proptest! {
     ) {
         do_i24_to_f32_roundtrip(i1);
     }
+
+    #[test]
+    fn i16_to_i24_via_f32_roundtrip(
+        i1 in any::<i16>()
+    ) {
+        do_i16_to_i24_via_f32_roundtrip(i1);
+    }
+
 }
