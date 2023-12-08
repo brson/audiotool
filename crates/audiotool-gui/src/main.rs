854696a09ae930a6b7a7e565cbc73d604bea1aba
Brian Anderson <andersrb@gmail.com>
2023-12-07 22:24:29 -0700

    guistart

---
+++ crates/audiotool-gui/src/main.rs
@@ -0,0 +1,14 @@
+use rx::prelude::*;
+
+slint::slint! {
+    export component HelloWorld {
+        Text {
+            text: "hello world";
+            color: green;
+        }
+    }
+}
+
+fn main() {
+    HelloWorld::new().unwrap().run().unwrap();
+}
