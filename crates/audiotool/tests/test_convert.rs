d47e5fa5753875450e8ede72e198790775a3d1b4
Brian Anderson <andersrb@gmail.com>
2023-12-12 22:35:27 -0700

    .

--- crates/audiotool/tests/test_convert.rs
+++ crates/audiotool/tests/test_convert.rs
@@ -137,3 +137,67 @@ fn basic() -> AnyResult<()> {
 
     Ok(())
 }
+
+struct SingleTestCase {
+    inprops: Props,
+    outformat: Format,
+}
+
+use rx::itertools::Itertools;
+
+fn all_single_test_cases() -> impl Iterator<Item = SingleTestCase> {
+    let channels = [1, 2]; // todo
+    let codecs = [Codec::Wav, Codec::Flac, Codec::Vorbis];
+    let bit_depths = [BitDepth::F32, BitDepth::I24, BitDepth::I16];
+    let sample_rates = [SampleRate::K192, SampleRate::K48];
+
+    let all_formats = codecs.into_iter()
+        .cartesian_product(bit_depths.into_iter())
+        .cartesian_product(sample_rates.into_iter())
+        .map(|((codec, bit_depth), sample_rate)| {
+            Format {
+                codec, bit_depth, sample_rate,
+            }
+        }).collect::<Vec<_>>();
+
+    let outformats = all_formats.iter().cloned();
+    let inprops = all_formats.into_iter()
+        .cartesian_product(channels.into_iter())
+        .map(|(format, channels)| {
+            Props {
+                channels, format,
+            }
+        });
+    
+    Option::<SingleTestCase>::None.into_iter()
+}
+
+fn run_single_test_case(test: SingleTestCase) -> AnyResult<()> {
+    let tempdir = rx::tempfile::tempdir()?;
+    let config = cvt::config::Config {
+        reference_tracks_dir: tempdir.path().join("in"),
+        reference_track_regex: S("\\.wav$"),
+        out_root_dir: tempdir.path().join("out"),
+        out_path_template: S("{{out_root_dir}}/{{relative_path}}/{{file_stem}}.{{format_ext}}"),
+        formats: vec![
+            test.outformat,
+        ]
+    };
+
+    std::fs::create_dir_all(&config.reference_tracks_dir)?;
+
+    let inprops = test.inprops;
+    let infile = config.reference_tracks_dir.join("test.wav");
+    let outfile = config.out_root_dir.join("test.wav");
+
+    let frames = 1024;
+
+    let inbuf = write_test_file(&infile, inprops, frames)?;
+    run_convert(config)?;
+    let (outprops, outbuf) = read_file(&outfile)?;
+
+    assert_eq!(inprops, outprops);
+    assert_eq!(inbuf, outbuf);
+
+    Ok(())
+}
